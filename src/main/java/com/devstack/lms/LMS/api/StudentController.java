e61c5fb8021996e1db7b2557f6e1a7428b9dfec1
education-bootcamp <education.seekerscloud@gmail.com>
2025-05-27 20:36:32 +0530

    2

---
+++ src/main/java/com/devstack/lms/LMS/api/StudentController.java
@@ -0,0 +1,26 @@
+package com.devstack.lms.LMS.api;
+
+import com.devstack.lms.LMS.dto.request.RequestStudentDto;
+import com.devstack.lms.LMS.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// http://localhost:9090/api/v1/students
+@RestController
+@RequestMapping("/api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @PostMapping // create -> save
+    public String createStudent(
+            @RequestBody RequestStudentDto requestStudentDto
+    ) {
+        studentService.createStudent(requestStudentDto);
+        return "Student created";
+    }
+}
